e7b2b7e739dca0dd691b306d32f0ac9e13f5b275
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-03-23 22:33:26 +0100

    initial

---
+++ src/components/Course.svelte
@@ -0,0 +1,51 @@
+<script>
+  import Stage from './Stage.svelte';
+  export let course;
+  export let relations;
+  export let stage;
+  let s;
+
+  function select(selected) {
+    stage = selected.id;
+    s = selected;
+  }
+
+  $: updateRelations(s);
+
+  function updateRelations(s) {
+    if (s) relations[course] = s.id;
+  }
+
+  const stages = fetch(`/db/courses/${course}/stages.json`).then((res) => {
+    return res.json();
+  });
+  $: loadById(stage);
+  async function loadById(id) {
+    if (id) {
+      const list = await stages;
+      s = list.find((s) => {
+        return s.id === id;
+      });
+    }
+  }
+</script>
+
+{#if s != null}
+  {#key stage}
+    <Stage stage={s} {course} bind:id={stage} />
+  {/key}
+{:else}
+  {#await stages}
+    Browse
+  {:then stages}
+    {#each stages as stage}
+      <div class="card shadow-lg">
+        <div class="font-size-20 text-accent-medium text-white-dm font-weight-semi-bold py-5">{stage.name}</div>
+        {stage.description}
+        <div class="mt-auto d-flex w-full justify-content-end">
+          <button class="btn btn-rounded btn-primary border-0 shadow-lg text-white " type="button" on:click={() => select(stage)}> Start </button>
+        </div>
+      </div>
+    {/each}
+  {/await}
+{/if}
---
+++ src/components/Stage.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { Swipe, SwipeItem } from 'svelte-swipe';
+  export let stage;
+  export let course;
+  export let id;
+  let length = 0;
+  const pages = fetch(`/db/courses/${course}/${id}.json`).then(async (res) => {
+    const json = await res.json();
+    length = json.length;
+    return json;
+  });
+  let active;
+
+  let carousel;
+
+  function handleNext(active) {
+    if (active === length - 1) {
+      id = id + 1;
+    } else {
+      carousel.nextItem();
+    }
+  }
+</script>
+
+<div class="w-full">
+  <h1 class="pt-20 text-accent-medium text-white-dm font-weight-semi-bold">{stage.name}</h1>
+</div>
+{#await pages}
+  Pages
+{:then pages}
+  <Swipe showIndicators={true} autoplay={false} bind:active_item={active} bind:this={carousel}>
+    {#each pages as page}
+      <SwipeItem>
+        <h4>{page.src}</h4>
+        <h5>means</h5>
+        {page.trans}
+      </SwipeItem>
+    {/each}
+  </Swipe>
+{/await}
+<div class="mt-auto d-flex w-full justify-content-end">
+  <button class="btn btn-rounded btn-primary border-0 shadow-lg text-white " type="button" on:click={() => handleNext(active)}> Next </button>
+</div>
+
+<style>
+  :root {
+    --sv-swipe-panel-height: 30rem;
+  }
+</style>
